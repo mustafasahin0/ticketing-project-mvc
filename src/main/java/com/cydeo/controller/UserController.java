11dd637781af12c47c9ae638e5555ebeae16c720
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-07-16 17:46:19 -0600

    Part-2

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -4,7 +4,6 @@ import com.cydeo.dto.UserDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
